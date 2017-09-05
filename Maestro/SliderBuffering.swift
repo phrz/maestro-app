c43aad311626cd40bcc4a1fa33837f1af2089cc1
kena du <rdu@smu.edu>
2017-09-05 13:35:03 -0500

    lesson content page

---
+++ Maestro/SliderBuffering.swift
@@ -0,0 +1,21 @@
+//
+//  SliderBuffering.swift
+//  Maestro
+//
+//  Created by Kena Du on 9/5/17.
+//  Copyright © 2017 Maestro. All rights reserved.
+//
+
+import UIKit
+
+class SliderBuffering: UIStackView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
