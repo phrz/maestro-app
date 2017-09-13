4aaead6ab2b8589a2263fbcb546e62d284d42c48
kena du <rdu@smu.edu>
2017-09-13 10:55:47 -0500

    quiz not finished

---
+++ Maestro/QuizContentView.swift
@@ -0,0 +1,21 @@
+//
+//  QuizContentView.swift
+//  Maestro
+//
+//  Created by Kena Du on 9/13/17.
+//  Copyright © 2017 Maestro. All rights reserved.
+//
+
+import UIKit
+
+class QuizContentView: UIStackView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
