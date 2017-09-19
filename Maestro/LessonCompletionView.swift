6e8407b8841ea42d2becc7bee9e28b5858ffe5fe
phrz <phrz@me.com>
2017-09-19 00:45:46 -0500

    Completion page WIP

---
+++ Maestro/LessonCompletionView.swift
@@ -0,0 +1,21 @@
+//
+//  LessonCompletionView.swift
+//  Maestro
+//
+//  Created by Paul Herz on 9/18/17.
+//  Copyright © 2017 Maestro. All rights reserved.
+//
+
+import UIKit
+
+class LessonCompletionView: LessonCardView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
